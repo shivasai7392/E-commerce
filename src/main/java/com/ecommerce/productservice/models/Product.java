0b325ed583efbe8c79669f0b6ba8d8f20d6e0d30
shivasai7392 <shivasai7392@gmail.com>
2023-12-01 08:40:49 +0530

    Db connection and creating tables for models

--- src/main/java/com/ecommerce/productservice/models/Product.java
+++ src/main/java/com/ecommerce/productservice/models/Product.java
@@ -1,15 +1,19 @@
 package com.ecommerce.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
     private double price;
     private int stock;
+    @ManyToOne
     private Category category;
 }
