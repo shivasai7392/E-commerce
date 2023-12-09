da7e4a18475a1d86e889db9c4742dba6fa4fa1a0
shivasai7392 <shivasai7392@gmail.com>
2023-12-09 19:24:19 +0530

    changing id type from string to UUID

--- src/main/java/com/ecommerce/productservice/models/BaseModel.java
+++ src/main/java/com/ecommerce/productservice/models/BaseModel.java
@@ -1,18 +1,25 @@
 package com.ecommerce.productservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private String id;
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "id", unique = true, nullable = false, updatable = false, columnDefinition = "binary(16)")
+    private UUID id;
     private Date createdAt;
     private Date updatedAt;
 }
